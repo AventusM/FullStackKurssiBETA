f9fd304b269d12cbd3ec29a08c3478e5765d1e88
Anton Moroz <anton.moroz@helsinki.fi>
2017-12-21 20:39:11 +0200

    23. tehty (siis myös 22. tehty). Ohjelma ilmoittaa kurssin yhteenlasketun tehtävien lukumäärän reducella

--- tokaViikko/kokoelmienrenderointi/src/index.js
+++ tokaViikko/kokoelmienrenderointi/src/index.js
@@ -13,11 +13,23 @@ const Osa = (props) => {
 const Sisalto = (props) => {
     //Destrukturointi, otetaan parametri suoraan Kurssi - literaalista sellaisenaan aaa.bbb.ccc ... jatkumon välttämiseksi
     const { osat } = props
-    //Yksi osa -> li (mapataan...)
+    const summaaja = (summaNYT, indeksinArvo) => summaNYT + indeksinArvo
+    //Luodaan taulukko osien tehtävistä erikseen, aloitetaan tyhjästä
+    const tehtLKMTaulu = []
+    osat.forEach(osa => {
+        tehtLKMTaulu.push(osa.tehtavia)
+    })
+    const tehtyjenLKM = tehtLKMTaulu.reduce(summaaja)
+    // console.log(tehtLKMTaulu) -> TOIMII
+    console.log(tehtyjenLKM) //-> LASKEE yllä saadun taulun summan
     return (
-        <ul>
-            {osat.map(osa => <Osa key={osa.id} nimi={osa.nimi} tehtavienlkm={osa.tehtavia} />)}
-        </ul>
+        <div>
+            <ul>
+                {/* //Yksi osa -> li (mapataan...) */}
+                {osat.map(osa => <Osa key={osa.id} nimi={osa.nimi} tehtavienlkm={osa.tehtavia} />)}
+            </ul>
+            yhteensä {tehtyjenLKM} tehtävää
+        </div>
     )
 }
 
