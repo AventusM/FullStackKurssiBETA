dcfba5c1c081b76447f4c6feaa22b476f21c897b
Anton Moroz <anton.moroz@helsinki.fi>
2017-12-28 01:09:44 +0200

    61. tehtävä tehty, blogilista jaettu useaan moduuliin

--- neljasViikko/blog/index.js
+++ neljasViikko/blog/index.js
@@ -4,40 +4,18 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const Blog = require('./models/blog') // Varmuuden vuoksi ... (?)
 
-const Blog = mongoose.model('Blog', {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-module.exports = Blog
-
+const blogsRouter = require('./controllers/blogs')
+//OSOITE SIIS EDELLEEN .../api/blogs eikä pelkkä .../ mitä voisi luulla kontrollerissa
+app.use('/api/blogs', blogsRouter) //Reittien lyhennys -> miinuksena voi käyttää vain tätä muotoa kaikissa reiteissä
 app.use(cors())
 app.use(bodyParser.json())
+app.use(express.static('build'))
 
-const mongoUrl =  'mongodb://127.0.0.1:27017'
+const mongoUrl = 'mongodb://127.0.0.1:27017'
 mongoose.connect(mongoUrl, { useMongoClient: true })
 mongoose.Promise = global.Promise
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 const PORT = 3003
 app.listen(PORT, () => {
