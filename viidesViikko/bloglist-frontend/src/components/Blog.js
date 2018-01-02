befdd61840576b443b1017d74d065d42f368c9c9
Anton Moroz <anton.moroz@helsinki.fi>
2018-01-02 08:55:10 +0200

    86. tehtävä tehty, css tehtävänannon näköinen, refaktorointi olisi ihan hyvä juttu jo skaalautumisen puolesta...

--- viidesViikko/bloglist-frontend/src/components/Blog.js
+++ viidesViikko/bloglist-frontend/src/components/Blog.js
@@ -3,14 +3,6 @@ import React from 'react'
 
 
 const Blog = ({ blog, user }) => {
-  
-  const blogStyle = {
-    paddingTop: 10,
-    paddingLeft: 2,
-    border: 'solid',
-    borderWidth: 1,
-    marginBottom: 5
-  }
 
   return (
     <div>
--- viidesViikko/bloglist-frontend/src/components/Togglable.js
+++ viidesViikko/bloglist-frontend/src/components/Togglable.js
@@ -43,11 +43,19 @@ class TogglableDiv extends React.Component {
   }
 
   render() {
+    const blogStyle = {
+      paddingTop: 10,
+      paddingLeft: 2,
+      border: 'solid',
+      borderWidth: 1,
+      marginBottom: 5
+    }
+
     const hideWhenVisible = { display: this.state.visible ? 'none' : '' }
     const showWhenVisible = { display: this.state.visible ? '' : 'none' }
 
     return (
-      <div>
+      <div style={blogStyle}>
         <div style={hideWhenVisible}>
           <div onClick={this.toggleVisibility}>{this.props.title} {this.props.author}</div>
         </div>
