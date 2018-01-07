2670cbab2c9bebbc11fe3fd976d07164f9611232
Anton Moroz <anton.moroz@helsinki.fi>
2018-01-07 10:55:42 +0200

    91. tehtävä tehty. Like-napin peruskäyttäytymistä testattu

--- viidesViikko/bloglist-frontend/src/components/SimpleBlog.test.js
+++ viidesViikko/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import SimpleBlog from './SimpleBlog'
 
-describe.only('<SimpleBlog />', () => {
+describe('<SimpleBlog />', () => {
   it('renders its contents', () => {
     const blog = {
       title: 'Password rules are bullshit',
@@ -17,4 +17,22 @@ describe.only('<SimpleBlog />', () => {
     expect(blogContentDiv.text()).toContain(blog.likes)
 
   })
+
+  it('clicking like button twice calls its event handler twice', () => {
+    const blog = {
+      title: 'Password rules are bullshit',
+      author: 'Jeff Atwood',
+      likes: 10
+    }
+
+    const mockHandler = jest.fn()
+    const simpleBlogComponent = shallow(<SimpleBlog blog={blog} onClick={mockHandler} />)
+    const likeAddingButton = simpleBlogComponent.find('button')
+    likeAddingButton.simulate('click')
+    likeAddingButton.simulate('click')
+
+    expect(mockHandler.mock.calls.length).toBe(2)
+
+  })
+
 })
