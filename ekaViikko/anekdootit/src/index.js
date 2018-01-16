cc8109506bf621f5f28dd246e591cc919740eb71
Anton Moroz <anton.moroz@helsinki.fi>
2018-01-17 00:27:52 +0200

    20. tehtävä tehty täysin jälkijunassa

--- ekaViikko/anekdootit/src/index.js
+++ ekaViikko/anekdootit/src/index.js
@@ -1,28 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Button = (props) => {
+const Button = ({ handleClick, text }) => {
     return (
-        <button onClick={props.handleClick}>
-            {props.text}
+        <button onClick={handleClick}>
+            {text}
         </button>
     )
 }
 
-const DisplayVotes = (props) => {
+const DisplayVotes = ({ votes }) => {
     return (
         <div>
-            has {props.votes} votes
+            has {votes} votes
         </div>
     )
 }
 
-const DisplayMostVotes = (props) => {
+const DisplayMostVotes = ({ anecdote, votes }) => {
     return (
         <div>
             <h2>anecdote with most votes:</h2>
-            <p>{props.anecdote}</p>
-            <DisplayVotes votes={props.votes} />
+            <div>{anecdote}</div>
+            <div>has {votes} votes</div>
         </div>
     )
 }
@@ -32,7 +32,7 @@ class App extends React.Component {
         super(props)
         this.state = {
             selectedAnecdote: 0,
-            allShownSoFar: []
+            votes: Array(this.props.anecdotes.length).fill(0)
         }
     }
 
@@ -42,62 +42,40 @@ class App extends React.Component {
         }
     }
 
-    asetaYksiLisaaNykyiseenIndeksiin = () => {
-        return () => {
-            this.setState({ allShownSoFar: this.state.allShownSoFar.concat(this.state.selectedAnecdote) })
-        }
+    addOne = () => {
+        const updatedVotes = this.state.votes.slice()
+        updatedVotes[this.state.selectedAnecdote] += 1
+        console.log(updatedVotes)
+        this.setState({
+            votes: updatedVotes
+        })
     }
 
-    mostFrequent = () => {
-        return () => {
-            let counts = {}
-            for (let i = 0; i < this.state.allShownSoFar.length; i++) {
-                let num = this.state.allShownSoFar[i]
-                counts[num] = counts[num] ? counts[num] + 1 : 1
-            }
-            console.log("If it hurts... " + counts[0])
-            console.log("Adding manpower... " + counts[1])
-            console.log("The first 90 percent..." + counts[2])
-            console.log("Any fool can write..." + counts[3])
-            console.log("Premature optimization..." + counts[4])
-            console.log("Debugging is twice as hard if youre stupid..." + counts[5])
-        }
+    maxVoteAnecdoteIndex = () => {
+        const maxVotes = Math.max(...this.state.votes)
+        console.log('max votes', maxVotes)
+        const indexOfMax = this.state.votes.indexOf(maxVotes)
+        console.log('index of max votes', indexOfMax)
+        console.log('anecdote', this.props.anecdotes[indexOfMax])
+        return indexOfMax //undefined ilman return value
     }
 
-    //Laskun idea
-    //Järjestetään olemassaoleva allShownSoFar - taulu
-    //Loop per indeksi O(n^2...)
-    //Otetaan talteen suurimman luvun omaava indeksi..?
     render() {
-        console.log(this.props.anecdotes.length)
-        const amountOfVotesForCurrentIndex = this.state.allShownSoFar.filter(luku => luku === this.state.selectedAnecdote).length
-        // const mostFrequent = function () {
-        //     let counts = []
-        //     for (let i = 0; i < this.state.allShownSoFar.length; i++) {
-        //         let num = this.state.allShownSoFar.length[i]
-        //         counts[num] = counts[num] ? counts[num] + 1 : 1
-        //     }
-        //     console.log("If it hurts... " + counts[0])
-        //     console.log("Adding manpower... " + counts[1])
-        //     console.log("The first 90 percent..." + counts[2])
-        //     console.log("Any fool can write..." + counts[3])
-        //     console.log("Premature optimization..." + counts[4])
-        //     console.log("Debugging is twice as hard if youre stupid..." + counts[5])
-        // }
-        console.log(this.mostFrequent.bind(this))
-
+        console.log(this.maxVoteAnecdoteIndex())
+        const votesForCurrent = this.state.votes[this.state.selectedAnecdote]
         return (
             <div>
                 {this.props.anecdotes[this.state.selectedAnecdote]}
-                <DisplayVotes votes={amountOfVotesForCurrentIndex} />
+                <DisplayVotes votes={votesForCurrent} />
                 <div>
-                    <Button handleClick={this.asetaYksiLisaaNykyiseenIndeksiin()}
+                    <Button handleClick={this.addOne}
                         text="vote" />
                     <Button handleClick={this.setRandomAnecdote(Math.floor(Math.random() * this.props.anecdotes.length))}
                         text="next anecdote" />
                 </div>
-                {/* <DisplayMostVotes anecdote={this.props.anecdotes[maxVotesIndex]}
-                    votes={maxVotes} /> */}
+                <div>
+                    <DisplayMostVotes anecdote={this.props.anecdotes[this.maxVoteAnecdoteIndex()]} votes={this.state.votes[this.maxVoteAnecdoteIndex()]} />
+                </div>
             </div >
         )
     }
@@ -109,7 +87,7 @@ const anecdotes = [
     'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
     'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
 ]
 
 ReactDOM.render(
